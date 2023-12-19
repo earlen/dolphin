f3cfe559ff358bdbaa1df7aee84cb33c3cb46ca2
earlen <71523027+earlen@users.noreply.github.com>
2023-12-19 12:59:38 -0800

    classes created

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,31 +1,35 @@
 import static spark.Spark.*;
+import org.apache.poi.ss.usermodel.*;
+import java.io.File;
+import java.io.FileInputStream;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        //This is required to allow GET and POST requests with the header 'content-type'
+        // This is required to allow GET and POST requests with the header
+        // 'content-type'
         options("/*",
                 (request, response) -> {
-                        response.header("Access-Control-Allow-Headers",
-                                "content-type");
+                    response.header("Access-Control-Allow-Headers",
+                            "content-type");
 
-                        response.header("Access-Control-Allow-Methods",
-                                "GET, POST");
-
+                    response.header("Access-Control-Allow-Methods",
+                            "GET, POST");
 
                     return "OK";
                 });
 
-        //This is required to allow the React app to communicate with this API
+        // This is required to allow the React app to communicate with this API
         before((request, response) -> response.header("Access-Control-Allow-Origin", "http://localhost:3000"));
 
-        //TODO: Return JSON containing the candies for which the stock is less than 25% of it's capacity
+        // TODO: Return JSON containing the candies for which the stock is less than 25%
+        // of it's capacity
         get("/low-stock", (request, response) -> {
             return null;
         });
 
-        //TODO: Return JSON containing the total cost of restocking candy
+        // TODO: Return JSON containing the total cost of restocking candy
         post("/restock-cost", (request, response) -> {
             return null;
         });
